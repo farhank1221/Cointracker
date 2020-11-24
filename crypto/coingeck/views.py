91b2de135f946abf74167acc2d12723092b39e50
farhank1221 <farhank1221@gmail.com>
2020-11-24 22:28:38 +0800

    url,endpoint added

--- crypto/coingeck/views.py
+++ crypto/coingeck/views.py
@@ -5,8 +5,9 @@ from json2html import *
 
 
 def tabet(request):
-    basic = requests.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false").json
-    extra = requests.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false').json
+    url = 'https://api.coingecko.com/api/'
+    endpoint = 'v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+    extra = requests.get(url+endpoint).json
     # return HttpResponse(r)
-    return render(request,'index.html',{'result':basic,'extra':extra})
+    return render(request,'index.html',{'extra':extra})
 
